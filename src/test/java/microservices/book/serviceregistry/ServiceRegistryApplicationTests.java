5c5224d92d852e033ec3057a2aa6b4b2120199af
jihoon <jihoon_yang@ksmartech.com>
2020-04-17 16:25:51 +0900

    first commit

---
+++ src/test/java/microservices/book/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.book.serviceregistry;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
